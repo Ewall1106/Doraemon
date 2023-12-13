bbf6998a1e4c07deec7d44ecafa22f02e78c844e
danielxiong <ewall1106@gmail.com>
2023-12-13 15:35:22 +0800

    feat: remove AppUpdater tempoary && create event  file

---
+++ src/main/application.ts
@@ -0,0 +1,11 @@
+import { ipcMain } from 'electron';
+
+export default class Application {
+  static init() {
+    ipcMain.on('ipc-example', async (event, arg) => {
+      const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
+      console.log(msgTemplate(arg));
+      event.reply('ipc-example', msgTemplate('pong'));
+    });
+  }
+}
--- src/main/main.ts
+++ src/main/main.ts
@@ -9,27 +9,24 @@
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
 import path from 'path';
-import { app, BrowserWindow, shell, ipcMain } from 'electron';
-import { autoUpdater } from 'electron-updater';
-import log from 'electron-log';
+import { app, BrowserWindow, shell } from 'electron';
+// import { autoUpdater } from 'electron-updater';
+// import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
+import Application from './application';
 
-class AppUpdater {
-  constructor() {
-    log.transports.file.level = 'info';
-    autoUpdater.logger = log;
-    autoUpdater.checkForUpdatesAndNotify();
-  }
-}
+// class AppUpdater {
+//   constructor() {
+//     log.transports.file.level = 'info';
+//     autoUpdater.logger = log;
+//     autoUpdater.checkForUpdatesAndNotify();
+//   }
+// }
 
 let mainWindow: BrowserWindow | null = null;
 
-ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
-});
+Application.init();
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -109,7 +106,7 @@ const createWindow = async () => {
 
   // Remove this if your app does not use auto updates
   // eslint-disable-next-line
-  new AppUpdater();
+  // new AppUpdater();
 };
 
 /**
