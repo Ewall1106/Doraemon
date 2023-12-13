9505ca9a17568956f24d82e6ca34393c320bdf3a
danielxiong <ewall1106@gmail.com>
2023-12-13 16:33:14 +0800

    eslint: allow console

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     '@typescript-eslint/no-shadow': 'error',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'off',
   },
   parserOptions: {
     ecmaVersion: 2022,
