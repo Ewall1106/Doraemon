cb141728398ce87041f29b8f2a2a641f28935b92
danielxiong <ewall1106@gmail.com>
2023-12-14 18:25:07 +0800

    feat: create comfyui_macos_install.py and run

--- scripts/comfyui_macos_install.sh
+++ scripts/comfyui_macos_install.sh
@@ -18,6 +18,7 @@ case "${OS_ARCH}" in
 esac
 
 # config
+GIT_CLONE_DIR="$(pwd)/ComfyUI"
 INSTALL_DIR="$(pwd)/installer_files"
 CONDA_ROOT_PREFIX="$(pwd)/installer_files/conda"
 INSTALL_ENV_DIR="$(pwd)/installer_files/env"
@@ -67,3 +68,17 @@ if [ ! -d "$GIT_CLONE_DIR" ]; then
     echo "Cloning Git repository..."
     git clone "$GIT_REPO_URL"
 fi
+
+# environment isolation
+export PYTHONNOUSERSITE=1
+unset PYTHONPATH
+unset PYTHONHOME
+export CUDA_PATH="$INSTALL_ENV_DIR"
+export CUDA_HOME="$CUDA_PATH"
+
+# activate installer env
+source "$CONDA_ROOT_PREFIX/etc/profile.d/conda.sh" # otherwise conda complains about 'shell not initialized' (needed when running in a script)
+conda activate "$INSTALL_ENV_DIR"
+
+# setup installer env
+python comfyui_macos_install.py $@
