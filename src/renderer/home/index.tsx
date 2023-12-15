1eab73b3fe0e5452d9c6912745cdb8f58aeaded2
danielxiong <ewall1106@gmail.com>
2023-12-15 21:59:03 +0800

    feat: comfyui模块前端界面开发

--- src/renderer/home/index.tsx
+++ src/renderer/home/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Button, Modal } from '@mantine/core';
+import React, { useState } from 'react';
+import { Button, Modal, Flex, Space, Card, Anchor, Grid, Image, Group, Text, Input, Title } from '@mantine/core';
+import { IconFile } from '@tabler/icons-react';
 import { useDisclosure } from '@mantine/hooks';
 
 import 'xterm/css/xterm.css';
@@ -7,6 +8,7 @@ import styles from './styles.module.scss';
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
+  const [comfyFilePath, setComfyFilePath] = useState(() => window.electron.store.get('COMFYUI_INSTALL_DIR'));
 
   const handleShell = (filePath: string) => {
     window.electron.ipcRenderer.sendMessage('ipc-execute-bash', filePath);
@@ -28,16 +30,16 @@ export default function Home() {
   };
 
   const handleRun = () => {
-    const COMFYUI_INSTALL_DIR = window.electron.store.get('COMFYUI_INSTALL_DIR');
-    if (COMFYUI_INSTALL_DIR) {
-      handleShell(COMFYUI_INSTALL_DIR);
+    if (comfyFilePath) {
+      handleShell(comfyFilePath);
     } else {
       window.electron.ipcRenderer.sendMessage('ipc-dialog-open');
       window.electron.ipcRenderer.once('ipc-dialog-open', (filePaths) => {
         const filePath = filePaths?.[0];
-        console.log(filePath);
+        if (!filePath) return;
+
         window.electron.store.set('COMFYUI_INSTALL_DIR', filePath);
-        // handleShell(filePath);
+        setComfyFilePath(filePath);
         handleModal();
       });
     }
@@ -45,17 +47,60 @@ export default function Home() {
 
   return (
     <div className={styles.home}>
+      <Grid mt="sm">
+        <Grid.Col span={6}>
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+            <Card.Section>
+              <Image
+                width="100%"
+                src="https://avatars.githubusercontent.com/u/121283862?v=4"
+                height={160}
+                alt="Norway"
+                style={{ objectFit: 'contain' }}
+              />
+            </Card.Section>
+
+            <Group mt="md" mb="xs">
+              <Title order={4}>ComfyUI启动器</Title>
+            </Group>
+
+            <Flex>
+              <Anchor size="sm" href="" target="_blank">
+                视频教程
+              </Anchor>
+              <Text size="sm" c="dimmed" px="2px">
+                |
+              </Text>
+              <Anchor size="sm" href="" target="_blank">
+                文章教程
+              </Anchor>
+            </Flex>
+
+            <Space h="md" />
+
+            <Flex align="center">
+              <Text size="sm">安装位置:</Text>
+              <Space w="sm" />
+              <Input style={{ width: '60%' }} size="xs" disabled placeholder="请选择安装地址" value={comfyFilePath} />
+
+              <Button size="xs" leftSection={<IconFile size={14} />} variant="default">
+                更换位置
+              </Button>
+            </Flex>
+
+            <Button color="blue" fullWidth mt="md" radius="md" onClick={handleRun}>
+              一键启动
+            </Button>
+          </Card>
+        </Grid.Col>
+      </Grid>
+
       <Modal opened={opened} onClose={close} title="确认安装到该目录">
         <div>{window.electron.store.get('COMFYUI_INSTALL_DIR')}</div>
         <Button variant="filled" onClick={handleInstall}>
           确认
         </Button>
       </Modal>
-
-      <Button variant="filled" onClick={handleRun}>
-        一键启动
-      </Button>
-      <div>{window.electron.store.get('COMFYUI_INSTALL_DIR')}</div>
     </div>
   );
 }
