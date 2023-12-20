e97fea8435a9f583ce8bc1a3f70f87e6a87264a5
danielxiong <ewall1106@gmail.com>
2023-12-20 11:01:27 +0800

    feat: model download

--- src/renderer/pages/comfyui/PluginModel.tsx
+++ src/renderer/pages/comfyui/PluginModel.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Button, Flex, Space, Card, Text, Input, Progress, Divider } from '@mantine/core';
+import { Button, Flex, Space, Card, Text, Input, Divider, CopyButton } from '@mantine/core';
 import { IconDownload, IconExternalLink, IconX } from '@tabler/icons-react';
 import { message, Popconfirm } from 'antd';
 import { useComfyStore } from '@/store';
@@ -7,10 +7,7 @@ import { useComfyStore } from '@/store';
 const { ipcRenderer } = window.electron;
 
 export function ModelDownlod({ item }) {
-  const [speed, setSpeed] = useState(0);
-  const [progress, setProgress] = useState(0);
   const [pathExist, setPathExist] = useState(false);
-  const [downloading, setDownloading] = useState(false);
   const [deleteLoading, setDeleteLoading] = useState(false);
   const installPath = useComfyStore((state) => state.installPath);
   const [messageApi, contextHolder] = message.useMessage();
@@ -25,27 +22,6 @@ export function ModelDownlod({ item }) {
 
     checkPathExists();
   }, []);
-
-  const handleDownload = async () => {
-    setDownloading(true);
-    const id = Date.now();
-    ipcRenderer.sendMessage('download.bigFile', { path: targetInstallPath, url: item.url, id });
-    ipcRenderer.on('download.bigFile', ({ status, speed, progress, id: _id }) => {
-      if (id !== _id) return;
-      if (status === 'downloading') {
-        setSpeed(speed);
-        setProgress(progress);
-      } else if (status === 'error') {
-        messageApi.open({
-          type: 'error',
-          content: '启动文件下载失败，请检测网络',
-        });
-        setDownloading(false);
-      } else {
-        setDownloading(false);
-      }
-    });
-  };
 
   const handleDelete = async () => {
     try {
@@ -75,18 +51,12 @@ export function ModelDownlod({ item }) {
             <Text size="sm" fw="bold">
               模型名称：{item.name}
             </Text>
-            <Text size="xs">{pathExist ? '已存在' : `下载进度:${progress}% 速度:${speed}KB/s`}</Text>
+            {pathExist && <Text size="xs">已存在</Text>}
           </Flex>
-          {!pathExist && (
-            <>
-              <Space h="2" />
-              <Progress style={{ width: '100%' }} value={progress} />
-            </>
-          )}
         </Flex>
         <Space w="md" />
         {!pathExist && (
-          <Button size="xs" leftSection={<IconDownload size={14} />} loading={downloading} onClick={handleDownload}>
+          <Button size="xs" leftSection={<IconDownload size={14} />} component="a" target="_blank" href={item.url}>
             下载
           </Button>
         )}
@@ -110,9 +80,9 @@ export function ModelDownlod({ item }) {
           variant="default"
           component="a"
           target="_blank"
-          href={item.url}
+          href={item.source}
         >
-          浏览器中下载
+          来源
         </Button>
       </Flex>
     </div>
@@ -121,6 +91,7 @@ export function ModelDownlod({ item }) {
 
 export default function PluginModel({ item }) {
   const installPath = useComfyStore((state) => state.installPath);
+  const targetInstallDir = `${installPath}/comfyui-portable/ComfyUI/${item?.modelList?.[0]?.targetInstallPath}`;
 
   return (
     <Card shadow="none" m="md" padding="lg" radius="md">
@@ -128,13 +99,15 @@ export default function PluginModel({ item }) {
         <Text size="sm" fw="bold">
           下载位置：
         </Text>
-        <Input
-          style={{ flexGrow: 1 }}
-          size="xs"
-          disabled
-          placeholder="请选择安装地址"
-          value={`${installPath}/comfyui-portable/ComfyUI/${item?.modelList?.[0]?.targetInstallPath}`}
-        />
+        <Input style={{ flexGrow: 1 }} size="xs" disabled placeholder="安装地址" value={targetInstallDir} />
+        <Space w="xs" />
+        <CopyButton value={targetInstallDir}>
+          {({ copied, copy }) => (
+            <Button size="xs" color={copied ? 'teal' : 'blue'} variant="outline" onClick={copy}>
+              {copied ? '复制成功' : '复制路径'}
+            </Button>
+          )}
+        </CopyButton>
       </Flex>
 
       {item?.modelList &&
