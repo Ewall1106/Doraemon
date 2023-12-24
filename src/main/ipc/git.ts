f481b5e93e861b8418297cb3bc73deb2c256ddfa
danielxiong <ewall1106@gmail.com>
2023-12-24 18:43:54 +0800

    fix: git clone && pull

--- src/main/ipc/git.ts
+++ src/main/ipc/git.ts
@@ -1,27 +1,21 @@
 import { ipcMain } from 'electron';
-import { simpleGit } from 'simple-git';
 import gitUrlParse from 'git-url-parse';
 import fs from 'node:fs';
 import git from 'isomorphic-git';
 import http from 'isomorphic-git/http/node';
 
 export const gitInit = () => {
-  ipcMain.handle('git.urlParse', async (event, args) => {
+  ipcMain.handle('git.urlParse', async (_event, args) => {
     const { url } = args;
     const parsedUrl = gitUrlParse(url);
     const { owner, name, resource, protocol } = parsedUrl;
     return { owner, name, resource, protocol };
   });
 
-  ipcMain.handle('git.clone', async (event, args) => {
+  ipcMain.handle('git.clone', async (_event, args) => {
     const { repoURL, targetDirectory } = args;
-    // const git = simpleGit();
-
-    // const dir = path.join(process.cwd(), 'test-clone');
-    // .then(console.log);
 
     try {
-      // await git.clone(repoURL, targetDirectory);
       await git.clone({ fs, http, dir: targetDirectory, url: repoURL });
       console.log('Git clone successful');
     } catch (error) {
@@ -29,13 +23,21 @@ export const gitInit = () => {
     }
   });
 
-  ipcMain.handle('git.pull', async (event, args) => {
+  ipcMain.handle('git.pull', async (_event, args) => {
     const { targetDirectory } = args;
-    const git = simpleGit(targetDirectory);
-
     try {
-      await git.reset(['--hard']);
-      await git.pull();
+      await git.pull({
+        fs,
+        http,
+        dir: targetDirectory,
+        singleBranch: true,
+        prune: true,
+        // hack: 没有author信息无法拉取成功
+        author: {
+          name: 'Your Name',
+          email: 'your.email@example.com',
+        },
+      });
       console.log('Git pull successful');
     } catch (error) {
       console.error('Error during git pull:', error);
